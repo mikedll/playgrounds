d8d126ec6d69414817a7f31db37aaf990cfefc52
Michael Rivera <soymrmike@gmail.com>
2024-03-03 17:41:25 -0800

    Using insertion sort on small inputs to merge sort.

--- java/src/main/java/com/mikedll/playgrounds/InsertionSort.java
+++ java/src/main/java/com/mikedll/playgrounds/InsertionSort.java
@@ -9,6 +9,19 @@ public class InsertionSort {
     Arrays.output(input);
   }
   
+  public void sortSublist(int[] input, int p, int r) {
+    int len = r-p+1;
+    for(int i=1; i<len; i++) {
+      int key = input[p+i];
+      int j = i-1;
+      while(j >= 0 && input[p+j] > key) {
+        input[p+j+1] = input[p+j];
+        j--;
+      }
+      input[p+j+1] = key;
+    }
+  }
+  
   public void sort(int[] input) {
     for(int i=1; i<input.length; i++) {
       int key = input[i];
--- java/src/main/java/com/mikedll/playgrounds/MergeSort.java
+++ java/src/main/java/com/mikedll/playgrounds/MergeSort.java
@@ -1,11 +1,17 @@
 package com.mikedll.playgrounds;
 
+import java.lang.NumberFormatException;
+
 public class MergeSort {
   
   public static void main(String[] args) {
+    MergeSort ms = new MergeSort();   
+    if(args.length == 2 && args[0].equals("withInsertionSort")) {
+      ms.withInsertionSort(args[1]);
+      return;
+    }
     int[] input = new int[] { 12, 3, 7, 9, 14, 6, 11, 2 };
 
-    MergeSort ms = new MergeSort();   
     ms.mergeSort(input, 0, input.length-1);
     
     System.out.print("<");
@@ -16,6 +22,37 @@ public class MergeSort {
       }
     }
     System.out.print("> \n");
+  }
+  
+  public void withInsertionSort(String kArg) {
+    int[] input = Arrays.getRandom(20, 40);
+    Arrays.output("input: ", input);
+    
+    int k;
+    try {
+      k = Integer.parseInt(kArg);
+    } catch(NumberFormatException ex) {
+      throw new RuntimeException(ex);
+    }
+    mergeSortWithInsertion(input, 0, input.length-1, k);
+    Arrays.output("sorted: ", input);
+  }
+  
+  public void mergeSortWithInsertion(int[] input, int p, int r, int k) {
+    if(p >= r) {
+      return;
+    }
+    
+    if((r-p+1) <= k) {
+      InsertionSort is = new InsertionSort();
+      is.sortSublist(input, p, r);
+      return;
+    }
+    
+    int q = (p + r)/2;
+    mergeSortWithInsertion(input, p, q, k);
+    mergeSortWithInsertion(input, q+1, r, k);
+    merge(input, p, q, r);
   }
   
   public void mergeSort(int[] input, int p, int r) {
