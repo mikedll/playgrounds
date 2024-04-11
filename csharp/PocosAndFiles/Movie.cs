cea1dfd4d458ce509a65af17126588ff1cf992d8
Michael Rivera <soymrmike@gmail.com>
2024-04-11 13:46:39 -0700

    Can read movies from files.

--- csharp/PocosAndFiles/Movie.cs
+++ csharp/PocosAndFiles/Movie.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+
 namespace Mikedll.PocosAndFiles.Model
 {
     public class Movie 
@@ -17,10 +19,54 @@ namespace Mikedll.PocosAndFiles.Model
         
         public void WriteToFile(string name)
         {
-            using (StreamWriter outputFile = new StreamWriter($"data/{name}.json"))
+            using (StreamWriter outputFile = new StreamWriter($"output/{name}.json"))
             {
-                outputFile.WriteLine($"{Title} - {PrettyReleaseDate}");
+                outputFile.WriteLine(JsonSerializer.Serialize(this));
             }
+        }
+        
+        public static Movie[] LoadMovies()
+        {
+            var movies = new List<Movie>();
+            int i = 1;
+            while(true)
+            {
+                var inputFile = $"data/movie{i}.json";
+                if(!File.Exists(inputFile))
+                {
+                    break;
+                }
+                using(StreamReader sr = new StreamReader(inputFile))
+                {
+                    string? line = sr.ReadLine();
+                    if(line != null)
+                    {
+                        Movie? movie = JsonSerializer.Deserialize<Movie>(line);
+                        if(movie != null)
+                        {
+                            movies.Add(movie);                        
+                        } 
+                        else 
+                        {
+                            Console.WriteLine($"Warning: Got null when deserializing from {inputFile}");
+                        }                        
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Warning: Got null from ReadLine in file {inputFile}");
+                    }
+                }
+                i++;
+            }
+            return movies.ToArray();
+        }
+        
+        public static Movie[] DefaultMovies() 
+        {
+            var m1 = new Movie { Title = "Warcraft", ReleaseDate = new DateTime(2016, 6, 15) };
+            var m2 = new Movie { Title = "Assassins Creed", ReleaseDate = new DateTime(2016, 8, 25) };
+            Movie[] movies = new Movie[] { m1, m2 };
+            return movies;
         }
     }  
 }
--- csharp/PocosAndFiles/Program.cs
+++ csharp/PocosAndFiles/Program.cs
@@ -1,8 +1,6 @@
 using Mikedll.PocosAndFiles.Model;
 
-var m1 = new Movie { Title = "Warcraft", ReleaseDate = new DateTime(2016, 6, 15) };
-var m2 = new Movie { Title = "Assassins Creed", ReleaseDate = new DateTime(2016, 8, 25) };
-Movie[] movies = new Movie[] { m1, m2 };
+Movie[] movies = Movie.LoadMovies();
 
 int i = 1;
 foreach(Movie movie in movies) {
