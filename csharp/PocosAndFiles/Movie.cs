04f5b8b3239f3709ecde75bf92c5a8ef81d87f98
Michael Rivera <soymrmike@gmail.com>
2024-04-11 13:28:35 -0700

    Writing movies to file.

--- csharp/PocosAndFiles/Movie.cs
+++ csharp/PocosAndFiles/Movie.cs
@@ -5,14 +5,22 @@ namespace Mikedll.PocosAndFiles.Model
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
         
+        public string PrettyReleaseDate {
+            get {
+                return ReleaseDate.ToString("MMMM dd, yyyy");
+            }
+        }
         public Movie() 
         {
            Title = "";
         }
         
-        public void WriteToFile(string filename) 
+        public void WriteToFile(string name)
         {
-        
+            using (StreamWriter outputFile = new StreamWriter($"data/{name}.json"))
+            {
+                outputFile.WriteLine($"{Title} - {PrettyReleaseDate}");
+            }
         }
     }  
 }
--- csharp/PocosAndFiles/Program.cs
+++ csharp/PocosAndFiles/Program.cs
@@ -4,8 +4,9 @@ var m1 = new Movie { Title = "Warcraft", ReleaseDate = new DateTime(2016, 6, 15)
 var m2 = new Movie { Title = "Assassins Creed", ReleaseDate = new DateTime(2016, 8, 25) };
 Movie[] movies = new Movie[] { m1, m2 };
 
+int i = 1;
 foreach(Movie movie in movies) {
-    Console.WriteLine($"{movie.Title} was released on {movie.ReleaseDate.ToString("MMMM dd, yyyy")}");  
+    Console.WriteLine($"{movie.Title} was released on {movie.PrettyReleaseDate}");  
+    movie.WriteToFile($"movie{i}");
+    i++;
 }
-
-// m.WriteToFile("movie.json");
